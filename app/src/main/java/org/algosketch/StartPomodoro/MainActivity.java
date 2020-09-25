12f989699c815034267a7e6835d4903c9cfebf9d
algosketch <sjy_9484@naver.com>
2020-09-26 06:21:32 +0900

    xml - id 설정 java - java와 xml 뷰 연결 및 일부 버튼 기능 구현

--- app/src/main/java/org/algosketch/StartPomodoro/MainActivity.java
+++ app/src/main/java/org/algosketch/StartPomodoro/MainActivity.java
@@ -2,13 +2,77 @@ package org.algosketch.StartPomodoro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView halfHourOption;
+    TextView oneHourOption;
+    Button cntUpBtn;
+    Button cntDownBtn;
+    TextView repetitionCnt;
+    Button startBtn;
+    int cnt = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        halfHourOption = findViewById(R.id.half_hour_option);
+        oneHourOption = findViewById(R.id.one_hour_option);
+        cntUpBtn = findViewById(R.id.btn_repetition_up);
+        cntDownBtn = findViewById(R.id.btn_repetition_down);
+        repetitionCnt = findViewById(R.id.repetition_cnt);
+        startBtn = findViewById(R.id.btn_start);
+
+        halfHourOption.setOnClickListener(new OptionOnClick());
+        oneHourOption.setOnClickListener(new OptionOnClick());
+        cntUpBtn.setOnClickListener(new RepetitionOnClick());
+        cntDownBtn.setOnClickListener(new RepetitionOnClick());
+        startBtn.setOnClickListener(new StartButtonOnClick());
+    }
+
+    class OptionOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(view == halfHourOption) {
+                halfHourOption.setTypeface(null, Typeface.BOLD);
+                oneHourOption.setTypeface(null, Typeface.NORMAL);
+            }
+            if(view == oneHourOption) {
+                oneHourOption.setTypeface(null, Typeface.BOLD);
+                halfHourOption.setTypeface(null, Typeface.NORMAL);
+            }
+        }
+    }
+
+    class RepetitionOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(view == cntUpBtn) {
+                ++cnt;
+            }
+            if(view == cntDownBtn) {
+                if(cnt > 1) --cnt;
+                else showToast("반복 횟수가 1보다 작을 수 없습니다.");
+            }
+            repetitionCnt.setText(Integer.toString(cnt));
+        }
+    }
+
+    class StartButtonOnClick implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            // TODO : new intent
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
