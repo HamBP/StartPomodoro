befd815c99f9789238bce49d0a60de94a0cf03fa
algosketch <sjy_9484@naver.com>
2020-09-30 06:32:55 +0900

    java - 알람 기능 빼고 완성했는데 코드 더러운 거 알고 수정할 예정이니까 너무 뭐라고 하지 말아줘

--- app/src/main/java/org/algosketch/StartPomodoro/TimerActivity.java
+++ app/src/main/java/org/algosketch/StartPomodoro/TimerActivity.java
@@ -3,19 +3,97 @@ package org.algosketch.StartPomodoro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimerActivity extends AppCompatActivity {
-    int cycleLength;
+    TextView stateTextView;
+    TextView currentTime;
+    TextView currentRepetition;
+    Button finishBtn;
+
+    int m, s;
     int cnt;
+    int currentCnt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        cycleLength = getIntent().getIntExtra("cycleLength", 30);
+        stateTextView = findViewById(R.id.state);
+        currentTime = findViewById(R.id.current_time);
+        currentRepetition = findViewById(R.id.current_repetition);
+        finishBtn = findViewById(R.id.btn_finish);
+
+        finishBtn.setOnClickListener(new FinishBtn());
+
+        final int cycleLength = getIntent().getIntExtra("cycleLength", 30);
         cnt = getIntent().getIntExtra("repetition", 1);
 
-        System.out.println("log : " + cycleLength + " " + cnt);
+        initActivity(cycleLength);
+
+        m = (cycleLength/30) * 25;
+        s = 0;
+        currentCnt = 1;
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 타이머가 0
+                        if(m == 0 & s == 1) {
+                            // 집중
+                            if(stateTextView.getText().toString().equals("집중")) {
+                                if(currentCnt == cnt) {
+                                    finish();
+                                }
+                                else {
+                                    stateTextView.setText(R.string.state_break);
+                                }
+                            }
+                            else { // 휴식
+                                ++currentCnt;
+                            }
+                        }
+
+                        if(s > 0) --s;
+                        else {
+                            s = 59;
+                            --m;
+                            --cnt;
+                        }
+
+                        currentTime.setText(m + ":" + s);
+                    }
+                });
+            }
+        };
+
+        timer.scheduleAtFixedRate(timerTask, 1000, 1000);
+    }
+
+    void initActivity(int cycleLength) {
+        currentRepetition.setText("1회/" + cnt + "회");
+
+        if(cycleLength == 30) currentTime.setText("25:00");
+        else currentTime.setText("50:00");
+    }
+
+    class FinishBtn implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
     }
 }
+
